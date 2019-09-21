cb41e40aff899b8a2a95a680004ea2402a855071
frederickwittman <39687162+frederickwittman@users.noreply.github.com>
2019-09-20 22:38:57 -0600

    Update lab02.js

--- lab02.js
+++ lab02.js
@@ -7,7 +7,10 @@ function fibonacci(seriesLength) {
 
 /* Invariant: The first seriesLength - 1 elements of the Fibonacci series are stored sequentially in the array fibonacci(seriesLength - 1).
 
-This is not true for the special case where the original argument is seriesLength == 2.  For each value of seriesLength for which there is a recursive function call and for seriesLength == 1, however, the invariant holds.  Note also that the invariant describes the state of affairs as the array is actually being filled out.  The details of what is happening as the function argument approaches 2, i.e. the technical details of how the compiler implements recursion, is  probably compiler/language dependent. */
+This is not true for the special case where the original argument is seriesLength == 2.  For each value of seriesLength for which there
+is a recursive function call and for seriesLength == 1, however, the invariant holds.  Note also that the invariant describes the state
+of affairs as the array is actually being filled out.  The details of what is happening as the function argument approaches 2, i.e. the
+technical details of how the compiler implements recursion, is  probably compiler/language dependent. */
 
 if (seriesLength >= 1) {
 array = [];
